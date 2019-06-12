330ae687839c077ec96b349419c6d025aadd1474
Max Baun <max@d3applications.com>
2019-06-12 08:14:01 -0400

    adding ssso logging

--- server/controllers/auth/sso.js
+++ server/controllers/auth/sso.js
@@ -2,25 +2,31 @@ const axios = require('axios');
 const {getSsoUrl} = require('../../utils/auth');
 
 module.exports = async request => {
-  const id = request.auth && request.auth.credentials ? request.auth.credentials.id : null;
-  let data = request.payload;
+  try {
+    const id = request.auth && request.auth.credentials ? request.auth.credentials.id : null;
+    let data = request.payload;
 
-  if (!id) {
-    return {
-      success: false
-    };
-  }
-
-  const user = await getUser(id);
-
-  const ssoUrl = await getSsoUrl(user, data.redirectTo);
-
-  return {
-    success: true,
-    data: {
-      ssoUrl
+    if (!id) {
+      return {
+        success: false
+      };
     }
-  };
+
+    const user = await getUser(id);
+
+    const ssoUrl = await getSsoUrl(user, data.redirectTo);
+
+    return {
+      success: true,
+      data: {
+        ssoUrl
+      }
+    };
+  } catch (error) {
+    console.log('SSO ERROR');
+    console.log(error);
+    return error;
+  }
 };
 
 function getUser(id) {
