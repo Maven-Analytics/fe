3de01359798918ded06abb10017c0fe3681bc764
Max Baun <max@d3applications.com>
2019-07-19 20:43:39 -0400

    fixing carousel bug

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -31,11 +31,38 @@ class Carousel extends Component {
       this.goToSlide(activeIndex);
     }
 
-    if (prevProps.options !== this.props.options) {
+    if (!this.objectsEqual(prevProps.options, this.props.options)) {
       this.initCarousel();
-    } else if (!prevProps.children.equals(this.props.children)) {
+    } /* else if (!prevProps.children.equals(this.props.children)) {
+      console.log('children changed')
       this.initCarousel();
+    } */
+  }
+
+  objectsEqual(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length !== bProps.length) {
+      return false;
     }
+
+    for (var i = 0; i < aProps.length; i++) {
+      var propName = aProps[i];
+
+      // If values of same property are not equal,
+      // objects are not equivalent
+      if (a[propName] !== b[propName]) {
+        return false;
+      }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
   }
 
   goToSlide(index) {
