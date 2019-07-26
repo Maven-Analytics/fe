f59aba07705be20765a826d9beefb8c33f2da8c7
Max Baun <max@d3applications.com>
2019-07-26 08:03:27 -0400

    course lessons

--- src/components/courseLessons.js
+++ src/components/courseLessons.js
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
-import RichText from './richText';
+import {click} from '../utils/componentHelpers';
+import { List } from 'immutable';
 
-const CourseLessons = ({lessons}) => {
-  return (
-    <div className="course-lessons">
-      <RichText content={lessons}/>
-    </div>
-  );
-};
+class CourseLessons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeLesson: null
+    };
+
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick(activeLesson) {
+    if (this.state.activeLesson === activeLesson) {
+      return this.setState({activeLesson: null});
+    }
+
+    return this.setState({activeLesson});
+  }
+
+  render() {
+    const {lessons} = this.props;
+
+    return (
+      <div className="course-lessons">
+        {lessons.map((lesson, index) => (
+          <div key={index} className={['course-lessons__lesson', this.state.activeLesson === index ? 'open' : ''].join(' ')}>
+            <button
+              onClick={click(this.handleToggleClick, index)}
+              id={`lesson-${lesson.get('title')}-toggle`}
+              aria-expanded={this.state.activeLesson === index}
+              aria-controls={`lesson-${lesson.get('title')}-dropdown`}
+            >
+              <strong>{index + 1}</strong>
+              <span>{lesson.get('title')}</span>
+            </button>
+            <div
+              id={`lesson-${lesson.get('title')}-dropdown`}
+              role="region"
+              hidden={this.state.activeLesson !== index}
+              aria-labelledby={`lesson-${lesson.get('title')}-toggle`}
+            >
+              <ul>
+                {lesson.has('lessons') && List.isList(lesson.get('lessons')) && lesson.get('lessons').map(l => (
+                  <li key={l}>{l}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 CourseLessons.propTypes = {
-  lessons: ImmutablePropTypes.map.isRequired
+  lessons: ImmutablePropTypes.list.isRequired
 };
 
 export default CourseLessons;
