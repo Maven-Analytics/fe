3114f27ea3da07ed66404f13c031eb98006cf4d3
Max Baun <max@d3applications.com>
2019-08-13 21:47:52 -0400

    adding error logging

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -94,6 +94,10 @@ class MavenApp extends App {
     Router.events.off('routeChangeComplete', this.handleRouteChange);
   }
 
+  componentDidCatch(error) {
+    console.log(error);
+  }
+
   handleRouteChange() {
     this.props.store.dispatch(stateActions.stateReset());
   }
