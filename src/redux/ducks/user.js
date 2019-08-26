639b86f0257fce74980c08baa431d287607ccff1
Max Baun <max@d3applications.com>
2019-08-26 13:51:40 -0400

    recommended paths & courses issue when saving

--- src/redux/ducks/user.js
+++ src/redux/ducks/user.js
@@ -58,7 +58,7 @@ export const selectors = {
   getRecommendedCourses: createSelector([getRecommendedCourses], r => r),
   getRecommendedPaths: createSelector([getRecommendedPaths], r => r),
   getRecommendedPathsForDisplay: createSelector([getUser, getRecommendedPaths, pathSelectors.getPaths], (user, recommended, paths) => {
-    recommended = user.has('recommended_paths') && user.get('recommended_paths').count() ? user.get('recommended_paths') : recommended;
+    recommended = user.get('recommended_paths') && user.get('recommended_paths').count() ? user.get('recommended_paths') : recommended;
 
     return recommended.map(r => {
       const path = paths.find(p => p.get('id') === r.get('id')) || Map();
@@ -70,7 +70,7 @@ export const selectors = {
     });
   }),
   getRecommendedCoursesForDisplay: createSelector([getUser, getRecommendedCourses, courseSelectors.getCourses], (user, recommended, courses) => {
-    recommended = user.has('recommended_courses') && user.get('recommended_courses').count() ? user.get('recommended_courses') : recommended;
+    recommended = user.get('recommended_courses') && user.get('recommended_courses').count() ? user.get('recommended_courses') : recommended;
 
     return recommended.map(r => {
       const course = courses.find(c => c.get('id') === r.get('id')) || Map();
