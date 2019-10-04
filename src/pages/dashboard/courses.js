729f566bbee1b7ef608ee4872bde01dc7d77d1d4
Max Baun <max@d3applications.com>
2019-10-04 12:04:12 -0400

    match score to course card on dashboard page

--- src/pages/dashboard/courses.js
+++ src/pages/dashboard/courses.js
@@ -7,6 +7,7 @@ import {bindActionCreators} from 'redux';
 import {actions as courseActions, selectors as courseSelectors} from '../../redux/ducks/courses';
 import {selectors as errorSelectors} from '../../redux/ducks/error';
 import {selectors as loadingSelectors} from '../../redux/ducks/loading';
+import {selectors as userSelectors} from '../../redux/ducks/user';
 import {actions as dashboardActions, selectors as dashboardSelectors} from '../../redux/ducks/dashboard';
 import DashboardLayout from '../../layouts/dashboard';
 import CourseFilters from '../../components/courseFilters';
@@ -15,6 +16,8 @@ import CourseCard from '../../components/courseCard';
 import Image from '../../components/image';
 import DashboardNoData from '../../components/dashboardNoData';
 import withAuthSync from '../../components/withAuthSync';
+import {getMatchScoreForCourse} from '../../utils/courseHelpers';
+import {prettyPercent} from '../../utils/componentHelpers';
 
 class DashboardCourses extends Component {
   componentDidMount() {
@@ -23,26 +26,26 @@ class DashboardCourses extends Component {
   }
 
   render() {
-    const {loading, progress} = this.props;
+    const {loading, progress, user} = this.props;
     const courses = progress.get('courses');
 
     return (
       <DashboardLayout sidebar={CourseFilters} showWelcome loading={loading} title="Self-Paced Courses" activeLink={2}>
         <DashboardGrid>
-          {loading === false && courses.count() > 0 && courses.map(course => (
-            <CourseCard
-              full
-              key={course.get('id')}
-              course={course}
-              progress={course.get('percentage_completed')}
-              recommended={course.get('recommended') ? 'Recommended for you' : null}
-            />
-          ))}
+          {loading === false &&
+            courses.count() > 0 &&
+            courses.map(course => (
+              <CourseCard
+                full
+                key={course.get('id')}
+                match={`${prettyPercent(getMatchScoreForCourse(course, user))}%`}
+                course={course}
+                progress={course.get('percentage_completed')}
+                recommended={course.get('recommended') ? 'Recommended for you' : null}
+              />
+            ))}
           {loading === false && courses.count() === 0 && process.browser ? (
-            <DashboardNoData
-              text="No courses found!"
-              className="grid-span-2"
-            >
+            <DashboardNoData text="No courses found!" className="grid-span-2">
               <Image
                 src="/static/img/dashboard-no-data-328.jpg"
                 wrapStyle={{
@@ -84,22 +87,30 @@ DashboardCourses.propTypes = {
   loading: PropTypes.bool,
   error: PropTypes.string,
   actions: PropTypes.objectOf(PropTypes.func),
-  progress: ImmutablePropTypes.map
+  progress: ImmutablePropTypes.map,
+  user: ImmutablePropTypes.map
 };
 
 const mapStateToProps = state => ({
   loading: loadingSelectors.getLoading(['COURSES_FILTER'])(state),
   error: errorSelectors.getError(['COURSES_FILTER'])(state),
-  progress: dashboardSelectors.getProgress(state)
+  progress: dashboardSelectors.getProgress(state),
+  user: userSelectors.getUser(state)
 });
 
-const mapDispatchToProps = function (dispatch) {
+const mapDispatchToProps = function(dispatch) {
   return {
-    actions: bindActionCreators({
-      ...courseActions,
-      ...dashboardActions
-    }, dispatch)
+    actions: bindActionCreators(
+      {
+        ...courseActions,
+        ...dashboardActions
+      },
+      dispatch
+    )
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withAuthSync(DashboardCourses));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withAuthSync(DashboardCourses));
