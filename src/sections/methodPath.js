6d771960ab45a8e014969b1d684d96e21cce182f
Max Baun <max@d3applications.com>
2019-11-19 18:24:30 -0500

    method path issue

--- src/sections/methodPath.js
+++ src/sections/methodPath.js
@@ -1,13 +1,9 @@
 import React, {Component, createRef} from 'react';
 import PropTypes from 'prop-types';
-import {Controller, Scene} from 'react-scrollmagic';
 import Link from 'next/link';
-import gsap from 'gsap';
 
 import Image from '../components/image';
 import MethodHeader from '../components/methodHeader';
-import Scroller from '../components/scroller';
-import TrackVisibility from '../components/trackVisibility';
 
 class MethodPath extends Component {
   constructor(props) {
@@ -15,7 +11,10 @@ class MethodPath extends Component {
 
     this.state = {
       progress: 0,
-      pathStyle: {},
+      pathStyle: {
+        strokeDashoffset: 0,
+        strokeDasharray: 0
+      },
       circle: {
         x: 0,
         y: 0
@@ -39,7 +38,7 @@ class MethodPath extends Component {
   }
 
   handleScroll() {
-    if (!this.endEl || !this.elEnd.current || !this.startEl || !this.startEl.current || !this.path || !this.path.current) {
+    if (!this.elEnd || !this.elEnd.current || !this.elStart || !this.elStart.current || !this.path || !this.path.current) {
       return;
     }
 
@@ -91,7 +90,7 @@ class MethodPath extends Component {
                   <g id="Home-Copy" transform="translate(-138.000000, -1924.000000)" >
                     <path ref={this.path} stroke="#F0F0F0" strokeWidth="5" d="M720,1924.51953 L720,1928.4 C720,1953.25281 699.852814,1973.4 675,1973.4 L186,1973.4 C161.147186,1973.4 141,1993.54719 141,2018.4 L141,2038 L141,2468 C141,2492.85281 161.147186,2513 186,2513 L815,2513 C839.852814,2513 860,2533.14719 860,2558 L860,3073 C860,3097.85281 839.852814,3118 815,3118 L186,3118 C161.147186,3118 141,3138.14719 141,3163 L141,3669 C141,3693.85281 161.147186,3714 186,3714 L815,3714 C839.852814,3714 860,3734.14719 860,3759 L860,3862" id="Master-path"></path>
                     <path style={pathStyle} stroke="url(#pathStrokeGradient)" strokeWidth="5" d="M720,1924.51953 L720,1928.4 C720,1953.25281 699.852814,1973.4 675,1973.4 L186,1973.4 C161.147186,1973.4 141,1993.54719 141,2018.4 L141,2038 L141,2468 C141,2492.85281 161.147186,2513 186,2513 L815,2513 C839.852814,2513 860,2533.14719 860,2558 L860,3073 C860,3097.85281 839.852814,3118 815,3118 L186,3118 C161.147186,3118 141,3138.14719 141,3163 L141,3669 C141,3693.85281 161.147186,3714 186,3714 L815,3714 C839.852814,3714 860,3734.14719 860,3759 L860,3862" id="Master-path"></path>
-                    <circle id="app-walkthrough-circle" strokeOpacity=".48" stroke="#F6FDA5" strokeWidth="8" fill="#23E3D7" cx={circle.x} cy={circle.y} r="6"></circle>
+                    {circle.x && circle.y ? <circle id="app-walkthrough-circle" strokeOpacity=".48" stroke="#F6FDA5" strokeWidth="8" fill="#23E3D7" cx={circle.x} cy={circle.y} r="6"></circle> : null}
                   </g>
                 </g>
               </svg>
