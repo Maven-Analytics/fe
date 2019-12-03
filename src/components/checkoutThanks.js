21020230f89b3ed0f11ff50a78d0f38d8e7035ef
Max Baun <max@d3applications.com>
2019-12-02 19:12:37 -0500

    updating cta on thanks to go to dashboard

--- src/components/checkoutThanks.js
+++ src/components/checkoutThanks.js
@@ -11,10 +11,10 @@ const CheckoutThanks = () => {
       <MaIcon icon="maven" />
       <h1>Welcome to Maven Analytics</h1>
       <h2>{'Now Let\'s Get Started!'}</h2>
-      <p>Take a quick 2-minute survey, and we’ll match you to the best courses & paths to help you reach your goals.</p>
+      <p>Head to your student dashboard to get started.</p>
       <p>
-        <Link href={Routes.WelcomeSurvey}>
-          <a>Start Survey</a>
+        <Link href={Routes.Dashboard}>
+          <a>Take Me To My Dashboard</a>
         </Link>
       </p>
     </div>
