c927f7a41ac50a990341b8cccf488febbdb6324b
Max Baun <max@d3applications.com>
2020-01-15 16:29:20 -0500

    error page copy

--- src/components/checkoutThanks.js
+++ src/components/checkoutThanks.js
@@ -5,12 +5,21 @@ import React from 'react';
 import {Routes} from '../routes';
 import MaIcon from './maIcon';
 
-const CheckoutThanks = ({linkHref, linkText, subtitle, title}) => {
+const CheckoutThanks = ({icon, linkHref, linkText, style, subtitle, subtitleFirst, title}) => {
   return (
-    <div className="checkout-thanks">
-      <MaIcon icon="maven" />
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
+    <div className="checkout-thanks" style={style}>
+      <MaIcon icon={icon} />
+      {subtitleFirst ? (
+        <>
+         <h2>{subtitle}</h2>
+         <h1>{title}</h1>
+        </>
+      ) : (
+        <>
+          <h1>{title}</h1>
+          <h2>{subtitle}</h2>
+        </>
+      )}
       <p>
         <Link href={linkHref}>
           <a>{linkText}</a>
@@ -21,13 +30,17 @@ const CheckoutThanks = ({linkHref, linkText, subtitle, title}) => {
 };
 
 CheckoutThanks.propTypes = {
+  icon: PropTypes.string,
   linkHref: PropTypes.string,
   linkText: PropTypes.string,
+  style: PropTypes.object,
   subtitle: PropTypes.string,
+  subtitleFirst: PropTypes.bool,
   title: PropTypes.string
 };
 
 CheckoutThanks.defaultProps = {
+  icon: 'maven',
   linkHref: Routes.Dashboard,
   linkText: 'Take Me To My Dashboard',
   subtitle: 'Now Let\'s Get Started!',
