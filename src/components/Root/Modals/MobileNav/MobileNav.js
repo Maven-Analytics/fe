930c4acd52cc35757025c2ab42612e40c04f4ce4
Max Baun <max@d3applications.com>
2020-02-20 14:10:18 -0500

    onlink click close mobile menu

--- src/components/Root/Modals/MobileNav/MobileNav.js
+++ src/components/Root/Modals/MobileNav/MobileNav.js
@@ -1,11 +1,12 @@
-import {CloseIcon, LinkList, MobileMenu, Modal, Offmenu} from 'maven-ui';
 import * as PropTypes from 'prop-types';
-import React from 'react';
-import {useTheme} from 'styled-components';
+
+import {CloseIcon, LinkList, MobileMenu, Modal, Offmenu} from 'maven-ui';
+import {menuLinksMain, menuLinksRegister, userMenuLinks} from '#root/routes';
 
 import LoggedIn from '#root/components/loggedIn';
 import LoggedOut from '#root/components/loggedOut';
-import {menuLinksMain, menuLinksRegister, userMenuLinks} from '#root/routes';
+import React from 'react';
+import {useTheme} from 'styled-components';
 
 const MobileNav = ({open, onClose: handleClose}) => {
   const theme = useTheme();
@@ -22,7 +23,8 @@ const MobileNav = ({open, onClose: handleClose}) => {
     >
       <Offmenu
         align="right"
-        closeAlign="right" closeIcon={<CloseIcon/>}
+        closeAlign="right"
+        closeIcon={<CloseIcon />}
         closeStyle={{margin: '1rem 3rem'}}
         onClose={handleClose}
         open={open}
@@ -30,12 +32,12 @@ const MobileNav = ({open, onClose: handleClose}) => {
         wrapperStyle={{backgroundColor: theme.nero}}
       >
         <MobileMenu>
-          <LinkList links={menuLinksMain} />
+          <LinkList links={menuLinksMain} onLinkClick={handleClose} />
           <LoggedIn>
-            <LinkList links={userMenuLinks} />
+            <LinkList links={userMenuLinks} onLinkClick={handleClose} />
           </LoggedIn>
           <LoggedOut>
-            <LinkList links={menuLinksRegister} />
+            <LinkList links={menuLinksRegister} onLinkClick={handleClose} />
           </LoggedOut>
         </MobileMenu>
       </Offmenu>
