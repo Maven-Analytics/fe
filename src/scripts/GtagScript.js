3e84c6cbd30d9777ed0ee24aa45f3443648a839a
Max Baun <max@d3applications.com>
2020-02-20 09:54:35 -0500

    ADDING LOGGING

--- src/scripts/GtagScript.js
+++ src/scripts/GtagScript.js
@@ -1,13 +1,14 @@
-import {memo} from 'react';
 import TagManager from 'react-gtm-module';
-
 import accessConfig from '#root/utils/accessConfig';
 import {canUseDOM} from '#root/utils/componentHelpers';
+import {memo} from 'react';
 
 const GtagScript = () => {
   if (!canUseDOM()) {
     return null;
   }
+
+  console.log('DISABLE_GTAG: ', accessConfig('DISABLE_GTAG'));
 
   if (!accessConfig('DISABLE_GTAG', false)) {
     TagManager.initialize({
