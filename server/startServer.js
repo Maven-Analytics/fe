9d070aab8fccffd9cedcf8d5b2d59e87fcea5907
Max Baun <max@d3applications.com>
2020-02-20 09:47:06 -0500

    adding hapi gate

--- server/startServer.js
+++ server/startServer.js
@@ -4,6 +4,7 @@ const Boom = require('@hapi/boom');
 const axios = require('axios');
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.PORT || 5000, 10);
+const REDIRECT_TO_WWW = process.env.REDIRECT_TO_WWW;
 
 // If (dev) {
 //   require('dotenv').config();
@@ -39,6 +40,13 @@ const server = Hapi.server({
 
 app.prepare().then(async () => {
   try {
+    if (REDIRECT_TO_WWW) {
+      await server.register({
+        register: require('hapi-gate'),
+        options: {www: true} // Will force https and www on all requests
+      });
+    }
+
     await server.register([
       // Require('./server/auth'),
       require('./setupRoutes')(app)
