f23332de8d78ba19ac8af2388faf7c495c273d5e
Max Baun <max@d3applications.com>
2020-03-19 08:41:19 -0400

    adding test env

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: path.resolve(process.cwd(), '.env.test')});
 
 Enzyme.configure({
   adapter: new Adapter()
