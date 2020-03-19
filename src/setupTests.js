a3497e9b0bf7837745943bc3bf077e01735a28df
Max Baun <max@d3applications.com>
2020-03-19 10:47:22 -0400

    Revert "adding test env"
    This reverts commit f23332de8d78ba19ac8af2388faf7c495c273d5e.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({path: path.resolve(process.cwd(), '.env.test')});
 
 Enzyme.configure({
   adapter: new Adapter()
