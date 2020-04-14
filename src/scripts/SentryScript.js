3c518cb9f208a2f0dc28860fb424467d4bc3c78b
Max Baun <max@d3applications.com>
2020-04-14 10:46:28 -0400

    setting user context for sentry

--- src/scripts/SentryScript.js
+++ src/scripts/SentryScript.js
@@ -1,17 +1,25 @@
 import * as Sentry from '@sentry/browser';
 import {memo} from 'react';
+import {useSelector} from 'react-redux';
 
 import accessConfig from '#root/utils/accessConfig';
+import {selectors as userSelectors} from '#root/redux/ducks/user';
 
 const SentryScript = () => {
   if (process.env.NODE_ENV === 'development') {
     return null;
   }
 
+  const user = useSelector(userSelectors.getUser);
+
   Sentry.init({dsn: 'https://fa3ec528363e494188ec3638755f3ce9@sentry.io/1862460'});
 
   Sentry.configureScope(scope => {
     scope.setTag('environment', accessConfig('SENTRY_ENVIRONMENT'));
+    scope.setUser({
+      email: user.get('email'),
+      id: user.get('id')
+    });
   });
 
   return null;
